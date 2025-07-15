0482282a42adb8139c2014918537415c939b3f1b
Gleb Aksenov <glebaksenov@MacBook-Pro-Gleb.local>
2025-07-15 18:38:56 +0300

    new structure

--- bot/database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from bot.config import DATABASE_URL
-
-engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
